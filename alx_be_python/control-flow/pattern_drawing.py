8146931de05abd084ff7459d6c6306f5ef3853a4
Jemimah Wachira <wachirajemimah36@gmail.com>
2025-06-06 22:43:04 +0300

    Add pattern drawing script with nested loops

---
+++ alx_be_python/control-flow/pattern_drawing.py
@@ -0,0 +1,9 @@
+size = int(input("Enter the size of the pattern: "))
+
+row = 0
+while row < size:
+    for _ in range(size):
+        print("*", end="")
+    print()  # Move to the next line after one row is printed
+    row += 1
+
